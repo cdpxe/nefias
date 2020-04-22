6e7849b79b67dec6a2a3256b2f62f87a4a4110c1
Steffen Wendzel <wendzel@hs-worms.de>
2020-04-22 16:07:20 +0200

    meta script improved

--- meta_mqtt_atrif_reconn.sh
+++ meta_mqtt_atrif_reconn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SLAVE_CONFIG=1slaves.cfg # both just sw-sun nefias1+2
+SLAVE_CONFIG=1slaves.cfg
 WAIT_SEC=5
 CHUNKSIZE=35000
 
@@ -9,24 +9,8 @@ FILES_WITHCC_QOS_ONE="`/bin/ls recordings/MQTT5_I5/Measurements/WithCC/QoS1/*.cs
 FILES_WITHOUTCC_QOS_NULL="`/bin/ls recordings/MQTT5_I5/Measurements/WithoutCC/QoS0/*.csv`"
 FILES_WITHOUTCC_QOS_ONE="`/bin/ls recordings/MQTT5_I5/Measurements/WithoutCC/QoS1/*.csv`"
 
-for recording in $FILES_WITHCC_QOS_NULL; do
-	# check file $num for WithCC AES 4T
-	./nefias_master.sh --add-values="mqtt.topic,mqtt.msgtype,mqtt.clientid" --slave-hostconfig=$SLAVE_CONFIG --slave-script=scripts/MQTT_ArtificialRecon_multiple_winsize.sh --traffic-source=${recording} --jobname="MQTT5_I4_`basename ${recording}`_$RANDOM" --allbusy-waitsec=$WAIT_SEC --chunk-size-in-lines=$CHUNKSIZE
+for recording in $FILES_WITHCC_QOS_NULL $FILES_WITHCC_QOS_ONE $FILES_WITHOUTCC_QOS_NULL $FILES_WITHOUTCC_QOS_ONE; do
+	./nefias_master.sh --add-values="mqtt.topic,mqtt.msgtype,mqtt.clientid" --slave-hostconfig=$SLAVE_CONFIG --slave-script=scripts/MQTT_ArtificialRecon_multiple_winsize.sh --traffic-source=${recording} --jobname="MQTT5_I5_`basename ${recording}`_$RANDOM" --allbusy-waitsec=$WAIT_SEC --chunk-size-in-lines=$CHUNKSIZE
 done
-
-for recording in $FILES_WITHCC_QOS_ONE; do
-	# check file $num for WithCC AES 4T
-	./nefias_master.sh --add-values="mqtt.topic,mqtt.msgtype,mqtt.clientid" --slave-hostconfig=$SLAVE_CONFIG --slave-script=scripts/MQTT_ArtificialRecon_multiple_winsize.sh --traffic-source=${recording} --jobname="MQTT5_I4_`basename ${recording}`_$RANDOM" --allbusy-waitsec=$WAIT_SEC --chunk-size-in-lines=$CHUNKSIZE
-done
-
-for recording in $FILES_WITHOUTCC_QOS_NULL; do
-	# check file $num for WithCC AES 4T
-	./nefias_master.sh --add-values="mqtt.topic,mqtt.msgtype,mqtt.clientid" --slave-hostconfig=$SLAVE_CONFIG --slave-script=scripts/MQTT_ArtificialRecon_multiple_winsize.sh --traffic-source=${recording} --jobname="MQTT5_I4_`basename ${recording}`_$RANDOM" --allbusy-waitsec=$WAIT_SEC --chunk-size-in-lines=$CHUNKSIZE
-done
-
-for recording in $FILES_WITHOUTCC_QOS_ONE; do
-	# check file $num for WithCC AES 4T
-	./nefias_master.sh --add-values="mqtt.topic" --slave-hostconfig=$SLAVE_CONFIG --slave-script=scripts/MQTT_ArtificialRecon_multiple_winsize.sh --traffic-source=${recording} --jobname="MQTT5_I4_`basename ${recording}`_$RANDOM" --allbusy-waitsec=$WAIT_SEC --chunk-size-in-lines=$CHUNKSIZE
-done
-
+exit 0
 
