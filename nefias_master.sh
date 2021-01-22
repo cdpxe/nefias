38cf34575b21b41de78442793de37c8e0cbd0aab
Steffen Wendzel <wendzel@hs-worms.de>
2021-01-22 19:41:46 +0100

    clean-up

--- nefias_master.sh
+++ nefias_master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 # This file is part of NeFiAS.
-# 
+#
+# (C) since 2020 Steffen Wendzel
+#
 # NeFiAS is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
